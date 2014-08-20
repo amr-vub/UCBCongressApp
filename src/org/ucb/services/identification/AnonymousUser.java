254a5a9f6169677d2371b857be28b1139655f19e
BEY9BLGW <BEY9BLGW@2522AT6R89Y125.bru.be.ibm.com>
2014-08-20 16:38:08 +0200

    Adapted AnonymousUser, RegisteredUser, Election. Added QuestionSystem and FeedbackSystem.

--- src/org/ucb/services/identification/AnonymousUser.java
+++ src/org/ucb/services/identification/AnonymousUser.java
@@ -2,29 +2,49 @@ package org.ucb.services.identification;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.ucb.data.dao.*;
 import org.ucb.data.domain.Access_Code_Dir;
 import org.ucb.data.domain.Agenda;
 import org.ucb.data.domain.HCP;
 import org.ucb.data.domain.HCPInitialInterests;
 
+@Service
 public class AnonymousUser {
-	
+
 	protected HCP hcpUser;
 	protected SessionIdentifierGenerator sessionIdentifier;
 	protected AccessCodeManager accessCodeManager;
+	protected AnonymousUserManager anonymousUserManager;
+	protected Access_Code_Dir accessCodeDir;
+
+	/**
+	 * Implicit constructor for derived classes
+	 */
+	public AnonymousUser() {
+
+	}
 
 	/**
 	 * Initialization constructor. To be used for the initial survey.
-	 * @param HCPID - the HCP's unique ID
-	 * @param specialization - the HCP's specialization
-	 * @param sub_specialization - the HCP's sub-specialization
-	 * @param academic_practitioner_value - the HCP's academic position
-	 * @param regesteredStatus - whether the HCP is registered or not
-	 * @param no_Sent_Invitation - the number of sent invitations
+	 * 
+	 * @param HCPID
+	 *            - the HCP's unique ID
+	 * @param specialization
+	 *            - the HCP's specialization
+	 * @param sub_specialization
+	 *            - the HCP's sub-specialization
+	 * @param academic_practitioner_value
+	 *            - the HCP's academic position
+	 * @param regesteredStatus
+	 *            - whether the HCP is registered or not
+	 * @param no_Sent_Invitation
+	 *            - the number of sent invitations
 	 */
-	public AnonymousUser(int HCPID, String specialization, String sub_specialization, String academic_practitioner_value, boolean regesteredStatus, int no_Sent_Invitation)
-	{
+	public AnonymousUser(int HCPID, String specialization,
+			String sub_specialization, String academic_practitioner_value,
+			boolean regesteredStatus, int no_Sent_Invitation) {
 		hcpUser.setHCPID(HCPID);
 		hcpUser.setSpecialization(specialization);
 		hcpUser.setSub_specialization(sub_specialization);
@@ -32,127 +52,236 @@ public class AnonymousUser {
 		hcpUser.setRegesteredStatus(regesteredStatus);
 		hcpUser.setNo_Sent_Invitation(no_Sent_Invitation);
 	}
-	
+
+	/**
+	 * Stores a new HCP User into the DB
+	 */
+	@Transactional
+	public void storeAnonymousUser() {
+		anonymousUserManager.storeAnonymousUser(hcpUser);
+	}
+
 	/**
 	 * Sets the HCP's Agenda
-	 * @param hCP_agenda - an Agenda object to be set
+	 * 
+	 * @param hCP_agenda
+	 *            - an Agenda object to be set
 	 */
-	public void setAgenda(Agenda hCP_agenda)
-	{
-		hcpUser.setHCP_agenda(hCP_agenda);
+	@Transactional
+	public void setAgenda(Agenda hCP_agenda) {
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+
+			hcp.setHCP_agenda(hCP_agenda);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
 	}
-	
-	
+
 	/**
 	 * Sets the HCP's initial interests
-	 * @param hCP_hcpInitialInterests - list of interests
+	 * 
+	 * @param hCP_hcpInitialInterests
+	 *            - list of interests
 	 */
-	public void setInitialInterests(List<HCPInitialInterests> hCP_hcpInitialInterests)
-	{
-		hcpUser.setHCP_hcpInitialInterests(hCP_hcpInitialInterests);
+	@Transactional
+	public void setInitialInterests(
+			List<HCPInitialInterests> hCP_hcpInitialInterests) {
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+
+			hcp.setHCP_hcpInitialInterests(hCP_hcpInitialInterests);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
 	}
-	
+
 	/**
-	 * Generates an invitation for another HCP to use the application.
-	 * @return the generated invitation code.
+	 * Generates a code which doesn't exist already as an user ID. For internal use only.
+	 * 
+	 * @return an unique code.
 	 */
-	public int generateInvitation(){
-		
-		int invitationCode        = 0;
-		int no_Sent_Invitation    = 0;
-		boolean codeExistsAlready = true;	
-		Access_Code_Dir accessCodeDir = null;
-		
-		/* Generate a unique invitation code*/
-		while(codeExistsAlready)
-		{
-			invitationCode = sessionIdentifier.generateCode().intValue();
-			
-			codeExistsAlready = accessCodeManager.accessCodeExists(invitationCode);
-			
-			if(codeExistsAlready == false)
-			{
-				/* Store the invitation code in the DB*/
-				accessCodeDir.setAccessCode(invitationCode);
-				accessCodeManager.insertAccessCode(accessCodeDir);
-				
-				/* Update the number of sent invitations*/
-				no_Sent_Invitation = hcpUser.getNo_Sent_Invitation();
-				no_Sent_Invitation++;		
-				hcpUser.setNo_Sent_Invitation(no_Sent_Invitation);
-				
-				return invitationCode;
-			}
-			else
-			{
+	@Transactional
+	private int generateUniqueCode() {
+		int uniqueCode = 0;
+		boolean codeExistsAlready = true;
+		// Access_Code_Dir accessCodeDir = null;
+
+		/* Generate a unique invitation code */
+		while (codeExistsAlready) {
+			uniqueCode = Integer.valueOf(sessionIdentifier.generateCode());
+
+			codeExistsAlready = accessCodeManager.accessCodeExists(uniqueCode);
+
+			if (codeExistsAlready == false) {
+				return uniqueCode;
+			} else {
 				continue;
 			}
 		}
-		return invitationCode;	
+		return uniqueCode;
 	}
-	
+
+	/**
+	 * Generates an invitation for another HCP to use the application.
+	 * 
+	 * @return the generated invitation code.
+	 */
+	@Transactional
+	public int generateInvitation() {
+
+		int invitationCode = 0;
+		int no_Sent_Invitation = 0;
+
+		/* Generate a unique invitation code */
+		invitationCode = generateUniqueCode();
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+
+			/* Store the invitation code in the DB */
+			accessCodeDir.setAccessCode(invitationCode);
+			accessCodeManager.insertAccessCode(accessCodeDir);
+
+			/* Update the number of sent invitations */
+			no_Sent_Invitation = hcp.getNo_Sent_Invitation();
+			no_Sent_Invitation++;
+			hcp.setNo_Sent_Invitation(no_Sent_Invitation);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
+
+		return invitationCode;
+	}
+
 	/**
 	 * Sets the HCP's specialization
-	 * @param specialization - the specialization to be set
+	 * 
+	 * @param specialization
+	 *            - the specialization to be set
 	 */
-	public void setSpecialization(String specialization)
-	{		
-		hcpUser.setSpecialization(specialization);
+	@Transactional
+	public void setSpecialization(String specialization) {
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+			hcp.setSpecialization(specialization);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
 	}
-	
+
 	/**
 	 * Set the sub-specialization
-	 * @param subSpecialization - the sub-specialization to be set
+	 * 
+	 * @param subSpecialization
+	 *            - the sub-specialization to be set
 	 */
-	public void setSubSpecialization(String subSpecialization)
-	{		
-		hcpUser.setSub_specialization(subSpecialization);
-		
+	@Transactional
+	public void setSubSpecialization(String subSpecialization) {
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+			hcp.setSub_specialization(subSpecialization);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
 	}
-	
+
 	/**
 	 * Sets the registered status
-	 * @param regesteredStatus - the current registration status to be set
-	 */
-	public void setRegesteredStatus(boolean regesteredStatus)
-	{		
-		hcpUser.setRegesteredStatus(regesteredStatus);		
-	}
-	
-	/**
 	 * 
+	 * @param regesteredStatus
+	 *            - the current registration status to be set
+	 */
+	@Transactional
+	public void setRegesteredStatus(boolean regesteredStatus) {
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+
+		if (hcp != null) {
+			hcp.setRegesteredStatus(regesteredStatus);
+
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
+	}
+
+	/**
+	 * Set the number of sent invitations
 	 * @param no_Sent_Invitation
 	 */
-	public void setNo_Sent_invitation(int no_Sent_Invitation)
-	{
-		hcpUser.setNo_Sent_Invitation(no_Sent_Invitation);
-	}
+	@Transactional
+	public void setNo_Sent_invitation(int no_Sent_Invitation) {
+		
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
+		
+		if(hcp != null)
+		{
+			hcp.setNo_Sent_Invitation(no_Sent_Invitation);
 	
+			/* Update the DB */
+			anonymousUserManager.updateAnonymousUser(hcp);
+		}
+	}
+
 	/**
 	 * This function checks an HCP in.
-	 * @param inputCode - the access code
-	 * @return 0 - if the user was in the system before
-	 * @return HCP_ID - if the user is a new-comer. The survey should follow in this case.
+	 * 
+	 * @param inputCode
+	 *            - the access code
+	 * @return 0 - if the user is already in the system
+	 * @return -1 - if the inputCode is wrong
+	 * @return HCP_ID - if the user is a new-comer. The survey should follow in
+	 *         this case.
 	 */
-	public int checkIn(int inputCode)
-	{
-		boolean codeExistsAlready     = true;
+	@Transactional
+	public int checkIn(int inputCode) {
+		boolean codeExists = true;
 		Access_Code_Dir accessCodeDir = null;
+
+		HCP hcp = anonymousUserManager
+				.findAnonymousUserById(hcpUser.getHCPID());
 		
-		codeExistsAlready = accessCodeManager.accessCodeExists(inputCode);
-		
-		if(codeExistsAlready == true)
+		if(null != hcp)
 		{
-			/* A returning user. */
+			// The user already exists
 			return 0;
 		}
+		
 		else
 		{
-			/* A new-comer*/
+			codeExists = accessCodeManager.accessCodeExists(inputCode);
+			
+			if (true != codeExists) {
+				//Error
+				return -1;
+			} 
+			
+			/* A new-comer */
 			accessCodeDir.setAccessCode(inputCode);
 			accessCodeManager.insertAccessCode(accessCodeDir);
-			
-			/* Redirect to survey.*/			
+
+			/* Redirect to survey. */
 			return inputCode;
 		}
 	}
